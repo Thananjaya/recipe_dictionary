5b98dacfe8dd43a573e08c9f0df89cf6bd06e235
thananjaya <thananjaya091993@gmail.com>
2020-08-29 21:18:51 +0530

    added model for serach module and loader logic been written

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base'; 
+import { elements, renderLoader, clearLoader } from './views/base'; 
 
 /** Global state of the app
  * - search object
@@ -15,8 +15,14 @@ const state = {
 const searchController = async () => {
     const query = searchView.getInput();
     if (query) {
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
         state.search = new Search(query);
+        searchView.clearInput();
+
         await state.search.getSearchResults();
+        clearLoader();
         searchView.displayResults(state.search.results);
     }
 }
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,6 +2,29 @@ import {elements} from './base';
 
 export const getInput = () => elements.searchInput.value;
 
+export const clearInput = () => {
+    elements.searchInput.value="";
+}
+
+export const clearResults = () => {
+    elements.searchResultsList.innerHTML = "";
+}
+
+const limitRecipeTitle = (title, limit = 25) => {
+    const shrimpedtitle =  [] 
+    if(title.length > limit){
+        title.split(' ').reduce((accumulator, currentValue) => {
+            if (accumulator + currentValue.length <= limit){
+                shrimpedtitle.push(currentValue)
+            }
+            return accumulator + currentValue.length;
+        },0);
+
+        return `${shrimpedtitle.join(' ')}...`;
+    }
+    return title;
+}
+
 const renderRecipe = recipe => {
     const markup = `
         <li>
@@ -10,7 +33,7 @@ const renderRecipe = recipe => {
                     <img src=${recipe.image_url} alt=${recipe.title}>
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
